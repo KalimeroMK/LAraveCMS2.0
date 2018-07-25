c82696a2b27192f405057053edee73c3b4ec1caf
Zoran <zbogoevski@gmail.com>
2018-07-25 11:41:25 +0200

    update to laravel 5.4 bug AppServiceProvider line 23 still not fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,29 +4,24 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app['rss'] = $this->app->share(function($app)
-        {
-            return new \Thujohn\Rss\Rss;
-        });
-
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// $this->app['rss'] = $this->app->share(function ($app) {
+		// 		return new \Thujohn\Rss\Rss;
+		// });
+	}
 }
